b1806e52d4940bc39c713aad6a465c2e22ff9a49
mnawara <marcinnawara@int.pl>
2025-01-14 23:32:33 +0100

    Create new component Divider

--- components/divider/Divider.tsx
+++ components/divider/Divider.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Divider = () => {
-  return <div className="w-[1px] opacity-15 bg-white h-full"></div>;
+const Divider = ({ position = "vertical", color = "bg-white" }) => {
+  const positionDivider =
+    position === "horizontal" ? "w-full h-[1px]" : "w-[1px] h-full";
+  return <div className={`opacity-15 ${color} ${positionDivider}`}></div>;
 };
 
 export default Divider;
