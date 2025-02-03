ab6c0d6b5c5648eb542d43085c08b170f45b7587
mnawara <marcinnawara@int.pl>
2025-02-03 23:41:39 +0100

    Add typescript for Category

--- components/category/Category.tsx
+++ components/category/Category.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const Category = ({ title, colorText = "text-blue-600" }) => {
+type CategoryProps = {
+  title: string;
+  colorText: string;
+};
+
+const Category = ({ title, colorText = "text-blue-600" }: CategoryProps) => {
   return (
     <p
       className={`flex gap-2  items-center before:content-['']  before:h-[2px] before:w-6 before:flex uppercase  before:bg-blue-600 text-sm font-semibold  ${colorText}`}
