8da1ee55bc931cf7113962a42018305afd65db10
mnawara <marcinnawara@int.pl>
2025-06-07 12:37:38 +0200

    Add new rule to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
   },
 ];
