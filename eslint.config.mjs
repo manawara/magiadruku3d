334841a78e5615d5e48708b7145076a2293bf850
mnawara <marcinnawara@int.pl>
2025-06-07 12:40:33 +0200

    Add new rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
     },
   },
 ];
