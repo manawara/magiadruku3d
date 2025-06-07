8fc3fb60c0a1d8e68a0dd3a3f9dfda07685ef076
mnawara <marcinnawara@int.pl>
2025-06-07 14:11:33 +0200

    Fix params as props

--- app/api/orders/[id]/tracking_number/route.ts
+++ app/api/orders/[id]/tracking_number/route.ts
@@ -4,20 +4,19 @@ const AUTH_TOKEN = process.env.FURGONETKA_API_TOKEN;
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const auth = req.headers.get("authorization");
   if (!auth || auth !== `Bearer ${AUTH_TOKEN}`) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
+  const { id } = await params;
   const data = await req.json();
   const trackingNumber = data.tracking_number;
 
   // Tu możesz np. zapisać tracking number w bazie danych
-  console.log(
-    `Odebrano tracking dla zamówienia ${params.id}: ${trackingNumber}`
-  );
+  console.log(`Odebrano tracking dla zamówienia ${id}: ${trackingNumber}`);
 
   return NextResponse.json({ status: "ok" });
 }
