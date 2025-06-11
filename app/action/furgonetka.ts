a856cf8b87e29f4e790190f84fe586472698307a
mnawara <marcinnawara@int.pl>
2025-06-11 09:58:46 +0200

    Configure next config for furgonetka

--- app/action/furgonetka.ts
+++ app/action/furgonetka.ts
@@ -4,8 +4,14 @@ interface Carrier {
   name: string;
   // dodaj inne pola zgodnie z API
 }
+
+const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://magiadruku3d.vercel.app"
+    : "https://magiadruku3d.vercel.app"; // lokal;
+
 export const fetchCarriers = async (): Promise<Carrier[]> => {
-  const response = await fetch("/api/carriers");
+  const response = await fetch(`${API_BASE_URL}/api/carriers`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch carriers");
--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,31 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // next.config.js
 
   images: {
     domains: ["res.cloudinary.com"], // Dodaj domenę Cloudinary
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // lub "http://localhost:3000" dla większego bezpieczeństwa
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
